db41149fa5d28317182b80b3cfaca757c3853841
Mike <mraccoondev@gmail.com>
2022-09-28 13:14:49 -0700

    add Practice using functions

---
+++ Programming Fundamentals in Swift/Exercises.playground/Pages/11. Practice using functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+var goldBars = 0
+
+func unlockTreasureChest(inventory: Int) -> Int {
+    inventory + 100
+}
+
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
+
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
+
+goldBars = unlockTreasureChest(inventory: goldBars)
+print(goldBars)
