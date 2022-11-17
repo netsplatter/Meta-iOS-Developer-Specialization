81fd8e1e8a9518b8767ea43da487ca9c4305c5c3
Mike <cyberenot@gmail.com>
2022-11-16 16:20:49 -0800

    add 2-build-a-rn-screen

---
+++ React Native/2-build-a-rn-screen/App.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { View } from 'react-native';
+
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import WelcomeScreen from './WelcomeScreen';
+
+export default function App() {
+  return (
+    <>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: '#495E57',
+        }}>
+        <LittleLemonHeader />
+        <WelcomeScreen />
+      </View>
+      
+      <View style={{ backgroundColor: '#495E57' }}>
+             <WelcomeScreen />
+        <LittleLemonFooter />
+      </View>
+    </>
+  );
+}
