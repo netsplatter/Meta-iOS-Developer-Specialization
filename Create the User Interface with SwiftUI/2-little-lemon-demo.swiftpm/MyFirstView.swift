a7e2dca004b5492a64d20b8a1d6e7e75d3168bbf
Mike <cyberenot@gmail.com>
2022-11-30 14:26:18 -0800

    add 2-little-lemon-demo

---
+++ Create the User Interface with SwiftUI/2-little-lemon-demo.swiftpm/MyFirstView.swift
@@ -0,0 +1,20 @@
+//
+//  MyFirstView.swift
+//  2-little-lemon-demo
+//
+//  Created by Mike on 2022-11-30.
+//
+
+import SwiftUI
+
+struct MyFirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyFirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyFirstView()
+    }
+}
