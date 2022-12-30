a6a13276127b1aa05d00cba0a0aaa1855e7045bd
Mike <cyberenot@gmail.com>
2022-12-30 14:54:10 -0800

    add 4-repetitive-tasks-with-loops

---
+++ Mobile Development and JavaScript/4-repetitive-tasks-with-loops.js
@@ -0,0 +1,47 @@
+// Task 1
+for (i = 1; i <= 6; i++) {
+    if (i == 6) {
+        console.log("Task 1: Counting completed!")
+    } else {
+        console.log(i)
+    }
+}
+
+// Task 2
+for (i = 5; i >= 0; i--) {
+    if (i == 0) {
+        console.log("Task 2: Countdown finised!")
+    } else {
+        console.log(i)
+    }
+}
+
+// Task 3
+var i = 0
+while (i <= 5) {
+    i += 1
+
+    if (i == 6) {
+        console.log("Task 3: Counting completed!")
+    } else {
+        console.log(i)
+    }
+}
+
+// Task 4
+while (i > 0) {
+    i -= 1
+
+    if (i == 0) {
+        console.log("Task 4: Countdown finised!")
+    } else {
+        console.log(i)
+    }
+}
+
+// Task 5
+var year = 2018
+while (year < 2023) {
+    console.log(year)
+    year += 1
+}
