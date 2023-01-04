0ec2171823436d63079312529b4d6e8c483ebf94
Mike <cyberenot@gmail.com>
2023-01-04 14:37:53 -0800

    add 7-arrays-and-objects

---
+++ Mobile Development and JavaScript/7-arrays-and-objects.js
@@ -0,0 +1,9 @@
+var clothes = []
+clothes.push("shoes", "sweater", "shirt", "pants", "shorts")
+clothes.pop()
+clothes.push("hat")
+console.log(clothes[2])
+var favCar = {}
+favCar.color = "green"
+favCar.convertible = true
+console.log(favCar)
